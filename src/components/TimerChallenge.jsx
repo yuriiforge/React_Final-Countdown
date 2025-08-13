8a37a2145b88e247f21dbedcb7230c76dfe7463b
Yurii <yurii.stepaniuk.dev@gmail.com>
2025-08-13 17:54:52 +0300

    Add timers functionality

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,40 @@
+import { useState, useRef } from 'react';
+
+const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef(null);
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} challenge
+        </button>
+      </p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
