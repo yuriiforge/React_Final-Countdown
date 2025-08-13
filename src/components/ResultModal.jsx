005a7d1b6afb03edb9094639c33921a9c8073ce9
Yurii <yurii.stepaniuk.dev@gmail.com>
2025-08-13 19:02:05 +0300

    Finish main functionality

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,11 @@
 import { useImperativeHandle, useRef } from 'react';
 
-const ResultModal = ({ ref, result, targetTime }) => {
+const ResultModal = ({ ref, remainingTime, targetTime, onReset }) => {
   const dialog = useRef();
+
+  const userLoast = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,14 +17,16 @@ const ResultModal = ({ ref, result, targetTime }) => {
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>Your {result}</h2>
+      {userLoast ? <h2>You lost!</h2> : <h2>Your score: {score}</h2>}
+
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
